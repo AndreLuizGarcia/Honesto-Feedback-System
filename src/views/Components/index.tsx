06601bcc1121aa019abf95bfb4e52ee6f58b4b57
AndreLuizGarcia <andre.luiz@gec.inatel.br>
2023-01-09 10:42:09 -0300

    feat(view): add components to be tested

--- src/views/Components/index.tsx
+++ src/views/Components/index.tsx
@@ -2,6 +2,28 @@ import Button from '../../components/Button'
 import MainLayout from '../../layouts/MainLayout'
 import Spinner from '../../components/Spinner'
 import User from '../../components/User'
+import Progress from '../../components/Progress'
+import TextArea from '../../components/TextArea'
+import Scale from '../../components/Scale'
+import MultipleChoice from '../../components/MultipleChoice'
+
+const questions = [
+  {
+    value: 1,
+    label: `Please Improve
+      You may have done one or the following: Maybe you were mostly quiet in meetings and when you had something on your mind, you brought it to the team afterward. Or, you had feedback that would be valuable to go, but you found it too difficult. Or, you had an opportunity to grow by doing something uncomfortable but you didn’t. `,
+  },
+  {
+    value: 2,
+    label: `You Were Good
+    You sometimes participate in meetings but you feel that they don’t always bring up important things when they should. `,
+  },
+  {
+    value: 3,
+    label: `You Were Great  
+      I and others can count on your courage to help the team do what is right. `,
+  },
+]
 
 const Components = () => (
   <MainLayout loggedIn>
@@ -25,6 +47,15 @@ const Components = () => (
     </Button>
 
     <Button
+      onClick={() => {
+        console.log('click')
+      }}
+      disabled
+    >
+      Primary button disabled
+    </Button>
+
+    <Button
       secondary
       onClick={() => {
         console.log('click')
@@ -37,6 +68,14 @@ const Components = () => (
     <div />
     <User name="John David" />
     <User name="John David" avatarUrl="https://i.pravatar.cc/150?img=68" />
+    <Progress
+      labelText="QUESTIONS COMPLETED"
+      completedItems={3}
+      totalItems={9}
+    />
+    <TextArea />
+    <MultipleChoice options={questions} />
+    <Scale />
   </MainLayout>
 )
 
