49fc0fd42640b76e2c71fa434b9bfb09e1bbeec4
AndreLuizGarcia <andre.luiz@gec.inatel.br>
2023-01-10 16:26:39 -0300

    feat(component): add NotFound component to be use with 404 route

--- src/views/NotFound/index.tsx
+++ src/views/NotFound/index.tsx
@@ -1,11 +1,24 @@
+import { useHistory } from 'react-router-dom'
+import Button from '../../components/Button'
+import MainLayout from '../../layouts/MainLayout'
 import styles from './notFound.module.css'
 
-const NotFound = () => (
-  <div className={styles.notFound}>
-    <h1>Page not found</h1>
-    <p>Sorry, the URL you have used may be incorrect.</p>
-    <p>Check the spelling or navigate using the menu above.</p>
-  </div>
-)
+const NotFound = () => {
+  const history = useHistory()
+  return (
+    <MainLayout>
+      <div className={styles.notFound}>
+        <h1>
+          <span>404</span>
+          <br />
+          Sorry! The page you are looking for cannot be found. 😢
+        </h1>
+        <Button onClick={() => history.push('/share-feedback')}>
+          Back to Share Feedback
+        </Button>
+      </div>
+    </MainLayout>
+  )
+}
 
 export default NotFound
