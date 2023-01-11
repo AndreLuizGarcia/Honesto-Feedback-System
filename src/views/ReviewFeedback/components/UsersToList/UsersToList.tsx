e4d0178c380d888e77889417d5673f974d70e825
AndreLuizGarcia <andre.luiz@gec.inatel.br>
2023-01-11 10:53:11 -0300

    feat: add users to list component - my feedback and team feedback page

---
+++ src/views/ReviewFeedback/components/UsersToList/UsersToList.tsx
@@ -0,0 +1,57 @@
+import * as React from 'react'
+import { useHistory, useLocation, useParams } from 'react-router-dom'
+import User from '../../../../components/User'
+import { SubmittedFeedbackContext } from '../../../../context/SubmittedFeedbackProvider'
+import { UserContext } from '../../../../context/UserProvider'
+import styles from './usersToList.module.css'
+import classnames from 'classnames'
+
+const UsersToList = () => {
+  const { feedbackFromLoggedUser, feedbackToLoggedUser } = React.useContext(
+    SubmittedFeedbackContext,
+  )
+  const users = React.useContext(UserContext)
+  const { id } = useParams<{ id: string | undefined }>()
+  const history = useHistory()
+  const { pathname } = useLocation()
+  const isTeamFeedbackPage = pathname.includes('/team-feedback')
+
+  const feedback = isTeamFeedbackPage
+    ? feedbackToLoggedUser
+    : feedbackFromLoggedUser
+
+  const usersToList = feedback.map((submittedAnswer, index) => {
+    const userGivenFeedback = users!.filter((user) =>
+      isTeamFeedbackPage
+        ? user.id === submittedAnswer.fromId
+        : user.id === submittedAnswer.toId,
+    )[0]
+
+    const historyToPush = isTeamFeedbackPage
+      ? `/team-feedback/${userGivenFeedback.id}`
+      : `/my-feedback/${userGivenFeedback.id}`
+
+    if (!id) {
+      history.push(historyToPush)
+    }
+
+    return (
+      <li
+        onClick={() => history.push(historyToPush)}
+        key={`usersToList-${index}`}
+        className={classnames(styles.user, {
+          [styles.active]: userGivenFeedback.id === id,
+        })}
+      >
+        <User
+          name={userGivenFeedback.name}
+          avatarUrl={userGivenFeedback.avatarUrl}
+        />
+      </li>
+    )
+  })
+
+  return <>{usersToList}</>
+}
+
+export default UsersToList
