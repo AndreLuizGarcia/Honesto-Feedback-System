238db81cac157aabaf409e4cd1450b87784f7e0e
AndreLuizGarcia <andre.luiz@gec.inatel.br>
2023-01-11 10:13:18 -0300

    feat(page): add giveFeedback page

--- src/views/GiveFeedback/index.tsx
+++ src/views/GiveFeedback/index.tsx
@@ -1,37 +1,45 @@
 import * as React from 'react'
-import { UserContext } from '../../context/UserProvider'
 import MainLayout from '../../layouts/MainLayout'
-import User from '../../components/User'
-import Button from '../../components/Button'
 import styles from './giveFeedback.module.css'
+import { useHistory, useParams } from 'react-router-dom'
+import ListOfUsers from './components/ListOfUsers'
+import BackArrow from '../../components/Icons/BackArrow'
+import FeedbackForm from './components/FeedbackForm'
+import { SubmittedFeedbackContext } from '../../context/SubmittedFeedbackProvider'
 
 const GiveFeedback = () => {
-  const users = React.useContext(UserContext)
+  const { usersThatDidntReceiveFeedbackAndAreNotLogged } = React.useContext(
+    SubmittedFeedbackContext,
+  )
+  const { id } = useParams<{ id: string | undefined }>()
 
   return (
     <MainLayout loggedIn>
-      <div className={styles.wrapper}>
-        <h1>Share Feedback</h1>
-        {users && users.length > 0 && (
-          <ul className={styles.users}>
-            {users.map((user) => (
-              <li key={user.id} className={styles.user}>
-                <User name={user.name} avatarUrl={user.avatarUrl} />
-                <span style={{ flex: 1 }} />
-                <Button
-                  onClick={() => {
-                    console.log('Fill out', user)
-                  }}
-                >
-                  Fill out
-                </Button>
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
+      {id ? (
+        <GiveFeedbackWrapper />
+      ) : (
+        <ListOfUsers users={usersThatDidntReceiveFeedbackAndAreNotLogged} />
+      )}
     </MainLayout>
   )
 }
 
 export default GiveFeedback
+
+const GiveFeedbackWrapper = () => {
+  const history = useHistory()
+
+  return (
+    <div className={styles.wrapper}>
+      <button
+        className={styles.goBackButton}
+        aria-label="Go to the previous page"
+        onClick={() => history.push('/share-feedback')}
+      >
+        <BackArrow />
+        BACK
+      </button>
+      <FeedbackForm />
+    </div>
+  )
+}
