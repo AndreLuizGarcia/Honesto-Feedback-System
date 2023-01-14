ceb8835d5b738ee69104fb4552a50878dcd4fb57
AndreLuizGarcia <andre.luiz@gec.inatel.br>
2023-01-13 21:02:35 -0300

    fix: removed oppositeNumber function

--- src/components/Scale/Scale.tsx
+++ src/components/Scale/Scale.tsx
@@ -14,15 +14,12 @@ const Scale = ({
   handleUpdateValue,
   ...props
 }: Props) => {
-  const [selected, setSelected] = React.useState<number>(() =>
-    oppositeNumber(initialValue),
-  )
+  const [selected, setSelected] = React.useState<number>(() => initialValue)
   const scale = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 
   function handleOnChange(index: number) {
     setSelected(index)
-    const realValue = oppositeNumber(index)
-    handleUpdateValue?.(realValue)
+    handleUpdateValue?.(index)
   }
 
   return (
@@ -31,27 +28,25 @@ const Scale = ({
         [styles.disabled]: disabled,
       })}
     >
-      {scale.map((_, index) => (
-        <React.Fragment key={`scale-${index}-${name}`}>
-          <input
-            type="radio"
-            name={name}
-            id={`scale-${index}-${name}`}
-            onChange={() => handleOnChange(index)}
-            checked={index === selected}
-            value={oppositeNumber(index)}
-            disabled={disabled}
-            {...props}
-          />
-          <label htmlFor={`scale-${index}-${name}`}></label>
-        </React.Fragment>
-      ))}
+      {scale.map((item) => {
+        return (
+          <React.Fragment key={`scale-${item}-${name}`}>
+            <input
+              type="radio"
+              name={name}
+              id={`scale-${item}-${name}`}
+              onChange={() => handleOnChange(item)}
+              checked={item === selected}
+              value={item}
+              disabled={disabled}
+              {...props}
+            />
+            <label htmlFor={`scale-${item}-${name}`}></label>
+          </React.Fragment>
+        )
+      })}
     </div>
   )
 }
 
 export default Scale
-
-function oppositeNumber(num: number) {
-  return 10 - num
-}
